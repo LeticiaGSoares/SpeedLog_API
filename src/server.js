d17b3783e5bbedd021e39cd61754bda74ef34c4a
DanielMoraisDev <danielfelipe2905@gmail.com>
2024-10-09 10:00:37 -0300

    :sparkles: Added index of routers

--- src/server.js
+++ src/server.js
@@ -3,10 +3,16 @@ import cors from "cors"
 import "dotenv/config"
 
 const PORT = process.env.PORT
+
 const app = express()
-app.use("/", (req, res)=>{
-    res.status(200).json({message: "Server rodando"})
-})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+import routers from "./routers.js"
+
+app.use("/", routers)
 
 app.use("*", (req, res)=> {
     res.status(404).json({message: "Rota não encontrada"})
